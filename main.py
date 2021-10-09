8209a6039dd4fc73969c8d02a764b6ce864afdf0
Pritamgadget <pritamgadget@gmail.com>
2021-10-09 22:13:06 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+import discord
+import os
+from discord.ext import commands
+from googletrans import Translator
+
+client=commands.Bot(command_prefix=['pls ', 'Pls '])
+
+deleted_messages = {}
+
+@client.command(aliases=['ts'])
+async def translate(ctx, *, inptext = None):
+    translator = Translator()
+    translated_text = translator.translate(inptext)
+    embed = discord.Embed(title="Translate", description = translated_text.text)
+    embed.set_footer(text=f"Source Langauge : '{translated_text.src}'")
+    await ctx.send(embed = embed)     
+
+@client.event
+async def on_ready():
+  await client.change_presence(status=discord.Status.idle, activity=discord.Game('with Toms Nana'))
+
+
+@client.event
+async def on_message_delete(message):
+    global deleted_messages
+    deleted_messages[message.channel.id] = {'author': message.author.name+'#'+message.author.discriminator, 'content': message.content, 'avatar_url': message.author.avatar_url}
+
+@client.command(aliases=['s'])
+async def snipe(ctx):
+    global deleted_messages
+    if ctx.message.channel.id in deleted_messages:
+        embed=discord.Embed(title="",description=f"{deleted_messages[ctx.message.channel.id]['content']}")    
+        embed.set_author(name="Sniper", icon_url=deleted_messages[ctx.message.channel.id]['avatar_url'])
+        embed.set_footer(text=f"Message deleted by {deleted_messages[ctx.message.channel.id]['author']}")
+    else:
+        embed=discord.Embed(title="Sniper",description="Nothing to snipe!")
+    await ctx.send(embed = embed)
+    
+
+client.run(os.getenv('TOKEN'))
