131c9230b919707494a4094681a984b983ebb45d
Pritamgadget <pritamgadget@gmail.com>
2021-10-14 21:35:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,27 @@ async def translate(ctx, *, inptext = None):
 async def on_ready():
   await client.change_presence(status=discord.Status.idle, activity=discord.Game('with Toms Nana'))
 
+@client.command()
+@commands.has_any_role('ADMIN, Vice Leader')
+async def poll(ctx, *, question=None):
+    if question == None:
+        await ctx.send("Please write a poll!")
+ 
+    icon_url = ctx.author.avatar_url 
+ 
+    pollEmbed = discord.Embed(title = "Poll", description = f"{question}")
+ 
+    pollEmbed.set_footer(text = f"Poll given by {ctx.author}", icon_url = ctx.author.avatar_url)
+ 
+    pollEmbed.timestamp = ctx.message.created_at 
+ 
+    await ctx.message.delete()
+ 
+    poll_msg = await ctx.send(embed = pollEmbed)
+ 
+    await poll_msg.add_reaction("❌")
+    await poll_msg.add_reaction("⭕")
+
 
 @client.event
 async def on_message_delete(message):
