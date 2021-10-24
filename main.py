e92543b0b50789d65551c42770fa957ed1a17ab3
Pritamgadget <pritamgadget@gmail.com>
2021-10-24 20:12:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import discord
 import os
 from discord.ext import commands
 from googletrans import Translator
+import giphy_client
+from giphy_client.rest import ApiException
+import random
 
 client=commands.Bot(command_prefix=['pls ', 'Pls ', 'p', 'P'])
 
@@ -57,5 +60,27 @@ async def snipe(ctx):
         embed=discord.Embed(title="Sniper",description="Nothing to snipe!")
     await ctx.send(embed = embed)
     
+@client.command()
+async def hug(ctx,*, member: discord.Member, q="hug"):
+
+    api_key="0XFxHlEGR4hUO7RxdHslVuqqmWf5kcRm"
+    api_instance = giphy_client.DefaultApi()
+
+    author_name = ctx.message.author.name
+
+    try: 
+    # Search Endpoint
+        
+        api_response = api_instance.gifs_search_get(api_key, q, limit=5, rating='r')
+        lst = list(api_response.data)
+        giff = random.choice(lst)
+
+        emb = discord.Embed(title=q)
+        emb.set_image(url = f'https://media.giphy.com/media/{giff.id}/giphy.gif')
+
+        await ctx.send (f'{author_name} has hugged {member.mention}')
+        await ctx.channel.send(embed=emb)
+    except ApiException as e:
+        print("Exception when calling DefaultApi->gifs_search_get: %s\n" % e)
 
 client.run(os.getenv('TOKEN'))
