a4319a42af3cadf97fc203088add86184402cb61
Pritamgadget <pritamgadget@gmail.com>
2021-10-26 18:15:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,61 @@ from giphy_client.rest import ApiException
 import random
 
 client=commands.Bot(command_prefix=['pls ', 'Pls ', 'p', 'P'])
+client.remove_command("help")
 
 deleted_messages = {}
+
+@client.group(invoke_without_command=True)
+async def help(ctx):
+  em = discord.Embed(title = "Help", description = "Use Pls help <command> for more info on a command ")
+
+  em.add_field(name = "Snipe", value = "snipe, s")
+  em.add_field(name = "Translate", value = "translate, ts")
+  em.add_field(name = "Poll", value = "poll, pollop")
+  em.add_field(name = "Fun", value = "hug, kick, lick, slap, punch, stare, kiss, highfive, bye")
+
+  await ctx.send(embed = em)
+
+@help.command(aliases=['Snipe, s, S'])
+async def snipe(ctx):
+
+  em = discord.Embed(title = "Snipe", description = "Snipes the last deleted message in channel", color = ctx.author.color)
+  em.add_field(name = "**Syntax**", value = "Pls snipe")
+
+  await ctx.send(embed = em)
+
+@help.command(aliases=['Translate, ts, Ts'])
+async def translate(ctx):
+
+  em = discord.Embed(title = "translate", description = "Translate any text in english", color = ctx.author.color)
+  em.add_field(name = "**Syntax**", value = "Pls translate/ts [message]")
+
+  await ctx.send(embed = em)
+
+@help.command(aliases=['Poll'])
+async def poll(ctx):
+
+  em = discord.Embed(title = "Poll", description = "Make a quick Yes or No poll", color = ctx.author.color)
+  em.add_field(name = "**Syntax**", value = "Pls poll [Question]")
+
+  await ctx.send(embed = em)
+
+@help.command(aliases=['Pollop'])
+async def pollop(ctx):
+
+  em = discord.Embed(title = "Option Poll", description = "Make a poll with four options by default ", color = ctx.author.color)
+  em.add_field(name = "**Syntax**", value = "Pls pollop [Question]")
+
+  await ctx.send(embed = em)
+
+@help.command(aliases=['Fun, hug, kick, lick, slap, punch, stare, kiss, highfive, bye'])
+async def fun(ctx):
+
+  em = discord.Embed(title = "Fun", description = "It's just as the title says", color = ctx.author.color)
+  em.add_field(name = "**Syntax**", value = "Pls [Any fun command] [@discord_User]")
+
+  await ctx.send(embed = em)
+
 
 @client.command(aliases=['ts'])
 async def translate(ctx, *, inptext = None):
@@ -22,7 +75,7 @@ async def translate(ctx, *, inptext = None):
 async def on_ready():
   await client.change_presence(status=discord.Status.idle, activity=discord.Game('with Toms Nana'))
 
-@client.command()
+@client.command(aliases=['Poll'])
 @commands.has_any_role('Spectrum members')
 async def poll(ctx, *, question=None):
     if question == None:
@@ -44,7 +97,7 @@ async def poll(ctx, *, question=None):
     await poll_msg.add_reaction("❌")
 
 
-@client.command()
+@client.command(aliases=['Pollop'])
 @commands.has_any_role('Spectrum members')
 async def pollop(ctx, *, question=None):
     if question == None:
@@ -67,7 +120,7 @@ async def pollop(ctx, *, question=None):
     await poll_msg.add_reaction("3️⃣")
     await poll_msg.add_reaction("4️⃣")
     
-@client.command()
+@client.command(aliases=['Hug'])
 async def hug(ctx,*, member: discord.Member, q="hug"):
 
     api_key="0XFxHlEGR4hUO7RxdHslVuqqmWf5kcRm"
@@ -90,7 +143,7 @@ async def hug(ctx,*, member: discord.Member, q="hug"):
     except ApiException as e:
         print("Exception when calling DefaultApi->gifs_search_get: %s\n" % e)
         
-@client.command()
+@client.command(aliases=['Kick'])
 async def kick(ctx,*, member: discord.Member, q="kicked"):
 
     api_key="0XFxHlEGR4hUO7RxdHslVuqqmWf5kcRm"
@@ -113,7 +166,7 @@ async def kick(ctx,*, member: discord.Member, q="kicked"):
     except ApiException as e:
         print("Exception when calling DefaultApi->gifs_search_get: %s\n" % e)
 
-@client.command()
+@client.command(aliases=['Lick'])
 async def lick(ctx,*, member: discord.Member, q="lick"):
 
     api_key="0XFxHlEGR4hUO7RxdHslVuqqmWf5kcRm"
@@ -136,7 +189,7 @@ async def lick(ctx,*, member: discord.Member, q="lick"):
     except ApiException as e:
         print("Exception when calling DefaultApi->gifs_search_get: %s\n" % e)
 
-@client.command()
+@client.command(aliases=['Slap'])
 async def slap(ctx,*, member: discord.Member, q="slapped"):
 
     api_key="0XFxHlEGR4hUO7RxdHslVuqqmWf5kcRm"
@@ -159,7 +212,7 @@ async def slap(ctx,*, member: discord.Member, q="slapped"):
     except ApiException as e:
         print("Exception when calling DefaultApi->gifs_search_get: %s\n" % e)
 
-@client.command()
+@client.command(aliases=['Punch'])
 async def punch(ctx,*, member: discord.Member, q="punched"):
 
     api_key="0XFxHlEGR4hUO7RxdHslVuqqmWf5kcRm"
@@ -182,7 +235,7 @@ async def punch(ctx,*, member: discord.Member, q="punched"):
     except ApiException as e:
         print("Exception when calling DefaultApi->gifs_search_get: %s\n" % e)
 
-@client.command()
+@client.command(aliases=['Stare'])
 async def stare(ctx,*, member: discord.Member, q="staring"):
 
     api_key="0XFxHlEGR4hUO7RxdHslVuqqmWf5kcRm"
@@ -205,30 +258,7 @@ async def stare(ctx,*, member: discord.Member, q="staring"):
     except ApiException as e:
         print("Exception when calling DefaultApi->gifs_search_get: %s\n" % e)
 
-@client.command()
-async def stab(ctx,*, member: discord.Member, q="stabbed"):
-
-    api_key="0XFxHlEGR4hUO7RxdHslVuqqmWf5kcRm"
-    api_instance = giphy_client.DefaultApi()
-
-    author_name = ctx.message.author.name
-
-    try: 
-    # Search Endpoint
-        
-        api_response = api_instance.gifs_search_get(api_key, q, limit=50, rating='r')
-        lst = list(api_response.data)
-        giff = random.choice(lst)
-
-        emb = discord.Embed(title=q)
-        emb.set_image(url = f'https://media.giphy.com/media/{giff.id}/giphy.gif')
-
-        await ctx.send (f'{author_name} stabbed {member.mention}')
-        await ctx.channel.send(embed=emb)
-    except ApiException as e:
-        print("Exception when calling DefaultApi->gifs_search_get: %s\n" % e)
-
-@client.command()
+@client.command(aliases=['Kiss'])
 async def kiss(ctx,*, member: discord.Member, q="kiss"):
 
     api_key="0XFxHlEGR4hUO7RxdHslVuqqmWf5kcRm"
@@ -251,7 +281,7 @@ async def kiss(ctx,*, member: discord.Member, q="kiss"):
     except ApiException as e:
         print("Exception when calling DefaultApi->gifs_search_get: %s\n" % e)
 
-@client.command()
+@client.command(aliases=['Highfive'])
 async def highfive(ctx,*, member: discord.Member, q="highfive"):
 
     api_key="0XFxHlEGR4hUO7RxdHslVuqqmWf5kcRm"
@@ -274,7 +304,7 @@ async def highfive(ctx,*, member: discord.Member, q="highfive"):
     except ApiException as e:
         print("Exception when calling DefaultApi->gifs_search_get: %s\n" % e)
 
-@client.command()
+@client.command(aliases=['Bye'])
 async def bye(ctx,*, member: discord.Member, q="bye"):
 
     api_key="0XFxHlEGR4hUO7RxdHslVuqqmWf5kcRm"
